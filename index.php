b6132a180032fa9e6d16ad061c1174db1bd0d666
chris <zhuxiangning@gmail.com>
2017-09-21 12:59:54 +0800

    - index.html 新增一行 - 新增 index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello | PHP</title>
+</head>
+<body>
+    <h1>Hello World, this is index.php</h1>
+    <?php echo "This is after hypertext preprocessor"
+    ?>
+</body>
+</html>
