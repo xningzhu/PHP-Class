a778b93cb4f04f00291025eab8c69782252d45ee
chris <zhuxiangning@gmail.com>
2017-09-21 13:41:45 +0800

    - 指定 $host, $user, $password 等變數的值 (127.0.0.1 或 localhost, user, secret) 成功登入。

--- connectToMySQL.php
+++ connectToMySQL.php
@@ -10,18 +10,20 @@
         echo "測試連線資料庫…";
 
         //註解：在 PHP 中以 dolor sign ($) 宣告變數
-        $host = "";
-        $user = "";
-        $password = "";
+        $host = "localhost";//請看主機 (Homestead) 上的 /etc/hosts，小心 Windows/system32/drivers/etc 之下也有 hosts 檔，要弄清楚編輯的對象
+        $user = "homestead";
+        $password = "secret";
 
+        //執行連線到資料庫的動作，並將回傳的東西存放到 $dbLink 中
         $dbLink = new mysqli($host, $user, $password);
 
+        //檢查是否連線錯誤
         if($dbLink->connect_error)
         {
             die("連線錯誤：".$dbLink->connect_error);
         }
-        else "連線成功囉！";
-
+        else
+            echo "<br><br><br>連線成功囉~";
     ?>
 </body>
 </html>
