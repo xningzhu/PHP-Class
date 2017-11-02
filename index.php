3bc7bab7d1bce5b2b8eb3cd1b0fdef859239b3a7
chris <zhuxiangning@gmail.com>
2017-11-02 16:42:25 +0800

    2017/11/02 Ending - 在 index.php 試用 nesbot\Carbon 這個 API 套件

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello | HTML</title>
+</head>
+<body>
+<h1>Hello World, this is index.html</h1>
+<br>
+<?php
+/*include_once __DIR__."/vendor/autoload.php";
+
+use Carbon\Carbon;
+
+//printf("Right now is %s", Carbon::now()->toDateTimeString());
+printf("台北現在時間：%s", Carbon::now())
+*/?>
+</body>
+</html>
